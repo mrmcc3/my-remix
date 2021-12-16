fcf8f66e78dd1d0c7f24bcd0f351068c3c99b756
Michael McClintock <michael.mcclintock@hey.com>
2021-12-17 07:08:33 +1000

    Update assets.js

--- src/assets.js
+++ src/assets.js
@@ -4,31 +4,27 @@ const manifest = JSON.parse(manifestJSON)
 
 export async function assetHandler(request, env) {
   if (request.method !== 'GET') return
-
-  // set content-type
   const url = new URL(request.url)
-  const headers = new Headers()
-  headers.set('Content-Type', mime.getType(url.pathname) || 'text/plain')
 
-  // development
-  if (process.env.NODE_ENV === 'development') {
-    // serve static assets in development with no caching
-    if (url.pathname.startsWith('/static/')) {
-      const body = await env.__STATIC_CONTENT.get(url.pathname, {
-        type: 'arrayBuffer'
-      })
-      return new Response(body, { headers })
-    }
+  // cloudflare pages
+  if (env.ASSETS) {
+    const res = await env.ASSETS.fetch(request)
+    if (res.status >= 400) return
+    const headers = new Headers(res.headers)
+    if (url.pathname.startsWith('/static/'))
+      headers.set('Cache-Control', 'public, max-age=31536000, immutable')
+    return new Response(res.body, { headers, status: res.status })
   }
-
-  // everything under static is assumed immutable
-  if (url.pathname.startsWith('/static/'))
-    headers.set('Cache-Control', 'public, max-age=31536000, immutable')
 
   // worker sites
   if (env.__STATIC_CONTENT) {
     const key = manifest[url.pathname.substring(1)]
     if (key) {
+      const headers = new Headers()
+      headers.set('Content-Type', mime.getType(url.pathname) || 'text/plain')
+      if (url.pathname.startsWith('/static/'))
+        headers.set('Cache-Control', 'public, max-age=31536000, immutable')
+
       const body = await env.__STATIC_CONTENT.get(key, {
         type: 'stream',
         cacheTtl: 86400
@@ -40,10 +36,15 @@ export async function assetHandler(request, env) {
     }
   }
 
-  // cloudflare pages
-  if (env.ASSETS) {
-    // TODO /static immutable
-    const res = await env.ASSETS.fetch(request)
-    if (res.status < 400) return res
+  // development
+  if (process.env.NODE_ENV === 'development') {
+    if (url.pathname.startsWith('/static/')) {
+      const headers = new Headers()
+      headers.set('Content-Type', mime.getType(url.pathname) || 'text/plain')
+      const body = await env.__STATIC_CONTENT.get(url.pathname, {
+        type: 'arrayBuffer'
+      })
+      return new Response(body, { headers })
+    }
   }
 }
